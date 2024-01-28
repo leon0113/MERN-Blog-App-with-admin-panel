8e0a76734f474688d6b120d471da35746302ec66
Leon <tahjib.leon399@gmail.com>
2024-01-28 23:49:47 +0600

    page details added

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 
 function Dashboard() {
     return (
-        <div>Dashboard</div>
+        <div>Dashboard Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error voluptate omnis perferendis asperiores reprehenderit numquam id tempora voluptas et assumenda minus similique commodi, exercitationem, iure hic magni ipsa magnam maxime neque? Impedit esse eum, facere consequuntur vitae neque natus assumenda non dolorem quibusdam exercitationem corrupti debitis eligendi et a quaerat?</div>
     )
 }
 
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,7 @@
 
 function SignIn() {
     return (
-        <div>SignIn</div>
+        <div>SignIn Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, aperiam ex. Blanditiis dignissimos nemo quidem dolores cumque eligendi magnam fuga reiciendis facilis dolore, doloribus officia alias quae sapiente ipsa ducimus pariatur totam aliquid sequi. Unde ea culpa necessitatibus reiciendis iste illo deleniti labore, blanditiis repudiandae commodi autem facere. Earum, amet.</div>
     )
 }
 
