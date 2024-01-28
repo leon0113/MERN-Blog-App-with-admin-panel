2db9537d3349b97fa2b9f46d763ba710db6f649a
Leon <tahjib.leon399@gmail.com>
2024-01-29 01:21:48 +0600

    Navbar done

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,68 @@
-import { Navbar } from 'flowbite-react'
-import { Link } from 'react-router-dom'
+import { Button, Navbar, TextInput } from 'flowbite-react'
+import { Link, useLocation } from 'react-router-dom'
+import { CiSearch } from "react-icons/ci";
+import { FaMoon } from "react-icons/fa";
 
 function Header() {
+    const path = useLocation().pathname;
     return (
         <Navbar className='border-b-2'>
+            {/* logo  */}
             <Link to={'/'} className='self-center text-sm sm:text-xl font-semibold dark:text-white px-2 py-1 rounded-lg bg-gradient-to-r from-indigo-300 via-purple-300 to-pink-300 '>
                 Mern Blog
             </Link>
+
+            {/* search input for large devices  */}
+            <form>
+                <TextInput
+                    type='text'
+                    placeholder='Search...'
+                    rightIcon={CiSearch}
+                    className='hidden lg:inline'
+                />
+            </form>
+
+            {/* mobile search button  */}
+            <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+                <CiSearch />
+            </Button>
+
+            {/* dark mode & signIn btn  */}
+            <div className='flex gap-2 md:order-2'>
+                {/* dark mode btn  */}
+                <Button className='w-12 h-10 hidden sm:inline' color='gray' pill >
+                    <FaMoon />
+                </Button>
+                {/* signIn btn  */}
+                <Link to={'/sign-in'}>
+                    <Button gradientDuoTone='purpleToBlue'>
+                        Sign In
+                    </Button>
+                </Link>
+
+                {/* mobile menu items  */}
+                <Navbar.Toggle />
+            </div>
+
+            {/* menu items  */}
+            <Navbar.Collapse>
+                <Navbar.Link active={path === '/'} as={'div'}>
+                    <Link to={'/'}>
+                        Home
+                    </Link>
+                </Navbar.Link>
+                <Navbar.Link active={path === '/about'} as={'div'}>
+                    <Link to={'/about'}>
+                        About
+                    </Link>
+                </Navbar.Link>
+                <Navbar.Link active={path === '/projects'} as={'div'}>
+                    <Link to={'/projects'}>
+                        Projects
+                    </Link>
+                </Navbar.Link>
+            </Navbar.Collapse>
+
         </Navbar>
     )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 export default function Home() {
     return (
-        <div>Home Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore illo nesciunt obcaecati a eveniet magni soluta tempora porro amet! Molestias, consequatur aperiam, expedita quod ad iure eaque ex quam vel similique ut ipsam eum natus iste facilis dolore quis consectetur deserunt porro voluptas repellendus sint. Id tenetur dolor consequuntur dicta.</div>
+        <div>Home</div>
     )
 }
