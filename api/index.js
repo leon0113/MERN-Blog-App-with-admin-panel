186d637db4d38148fdb78953256f55eb9c760ea3
Leon <tahjib.leon399@gmail.com>
2024-02-02 02:43:39 +0600

    Sign up functionality DONE

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cors from 'cors';
+
 
 dotenv.config();
 
@@ -14,6 +16,7 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000!");
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,49 @@
-import { Button, Label, TextInput } from "flowbite-react"
-import { Link } from "react-router-dom"
+/* eslint-disable no-unused-vars */
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react"
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom"
 
 function SignUp() {
+
+    const [formData, setFormData] = useState({});
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+    };
+    // console.log(formData);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!formData.userName || !formData.email || !formData.password) {
+            return setErrorMessage("Please fill all the fields");
+        }
+
+        try {
+            setLoading(true);
+            setErrorMessage(null);
+            const res = await fetch('/api/auth/signup', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json();
+
+            if (data.success === false) {
+                setLoading(false);
+                return setErrorMessage(data.message);
+            }
+            setLoading(false);
+            if (res.ok) navigate('/sign-in');
+        } catch (error) {
+            setLoading(false);
+            setErrorMessage("Something went wrong!");
+        }
+    }
+
     return (
         <div className="min-h-screen mt-20">
             <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-6">
@@ -14,13 +56,14 @@ function SignUp() {
                 </div>
                 {/*  right side div  */}
                 <div className="flex-1">
-                    <form className="flex flex-col gap-4">
+                    <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
                         <div>
                             <Label value="Enter your  username:" />
                             <TextInput
                                 type="text"
                                 placeholder="Username"
-                                id="username"
+                                id="userName"
+                                onChange={handleChange}
                             />
                         </div>
                         <div>
@@ -29,23 +72,41 @@ function SignUp() {
                                 type="text"
                                 placeholder="example@gmail.com"
                                 id="email"
+                                onChange={handleChange}
                             />
                         </div>
                         <div>
                             <Label value="Enter your  Password:" />
                             <TextInput
-                                type="text"
+                                type="password"
                                 placeholder="Password"
                                 id="password"
+                                onChange={handleChange}
                             />
                         </div>
 
-                        <Button gradientDuoTone='purpleToPink' type="submit">Sign Up</Button>
+                        <Button gradientDuoTone='purpleToPink' type="submit" disabled={loading}>
+                            {
+                                loading ? (
+                                    <>
+                                        <Spinner size="sm" />
+                                        <span className="pl-3">Loading...</span>
+                                    </>
+                                ) : "Sign Up"
+                            }
+                        </Button>
                     </form>
                     <div className="flex gap-2 text-sm mt-3">
                         <span>Have an account?</span>
                         <Link to="/sign-in" className="text-blue-500">Sign In</Link>
                     </div>
+                    {
+                        errorMessage && (
+                            <Alert className="mt-5" color='failure'>
+                                {errorMessage}
+                            </Alert>
+                        )
+                    }
                 </div>
             </div>
         </div>
